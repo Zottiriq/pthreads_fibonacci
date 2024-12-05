ccc576af2a2f46e7cb45c17c7655a40630ec10e5
Mehmet Kerem Inci <124329989+Zottiriq@users.noreply.github.com>
2024-12-06 01:19:00 +0300

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,58 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// fibonacciyi hesaplayan fonksiyon
+int fib(int b) {
+    if (b == 1){
+        return 1;
+    }
+    else if (b == 0) {
+        return 0;
+    }
+    else {
+        return fib(b-2) + fib(b-1);
+    }
+}
+
+// fibonacciyi threadda çalıştıracak fonksiyon
+void *fonksiyon(void *in) {
+    int a = (int)in;
+    int sonuc = fib(a);
+    printf("%d ", sonuc);
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int j;
+
+// thread içinde fibonacciyi hesaplayacak değer
+    int sayi;
+    pthread_attr_t özel;
+
+// kullanıcının birden fazla sayı girip girmediğini kontrol eden fonksiyon
+    if (argc != 2) {
+        printf("ölçüm için tek bir sayi girin");
+        return -1;
+    }
+
+// kullanıcının girdiği sayıyı integera çeviren kod
+    sayi = atoi(argv[1]);
+
+// kullanıcının girdiği sayı birden büyük mü ölçen fonksiyon
+    if (sayi < 1) {
+        printf("sayac 1 den büyük olmalidir");
+        return -1;
+    }
+
+//thread özelliklerini başlatan fonksiyon
+    pthread_attr_init(&özel);
+
+// 1 den sayac değerine kadar threadlar kullanarak fibonacci hesaplatacak fonksiyon
+    for (int j = 0; j <= sayi; j++) {
+        pthread_t thread;
+        pthread_create(&thread, &özel, fonksiyon, (void *)j);
+        pthread_join(thread, NULL);
+    }
+    return 0;
+}
